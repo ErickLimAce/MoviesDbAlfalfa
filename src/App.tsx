e2df9f33468bde14db270de6c349785e9e1d4142
ArturoCarballoArias <a01662245@tec.mx>
2024-04-10 22:29:38 -0600

    Aqui tienes Moy puta

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,21 @@ import { movies } from './components/constants/moviesMock';
 
 function App() {
   return (
-    <div>
-     <MovieCard
-      movieId={movies[0].id}
-      posterPath={movies[0].poster_path}
-      title={movies[0].title}
-      voteAverage={movies[0].vote_average}
-      genreId={movies[0].genre_ids[0]}
-     />
+    <div className='block pl-7'>
+      <div className='table max-w-[100%]'>
+        {movies.map((movie, index) => (
+          <MovieCard
+          key={movie.id}
+          movieId={movie.id}
+          posterPath={movie.poster_path}
+          title={movie.title}
+          voteAverage={movie.vote_average}
+          genreId={movie.genre_ids[0]}
+        />
+        ))}
+      </div>
     </div>
+
   );
 }
 
--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,6 +1,8 @@
 import { IMovieCard } from "./types";
 import { Pill } from "../Pill";
 import { IMAGE_SOURCE } from "../constants/moviesMock";
+import genresData from '../constants/genres.json';
+import './MovieCard.css'
 
 const MovieCard: React.FC<IMovieCard> = ({
     title,
@@ -11,22 +13,23 @@ const MovieCard: React.FC<IMovieCard> = ({
 }) => {
     const poster = IMAGE_SOURCE + posterPath;
 
-    const getGenre = (genreId: number): string => {
-        return "genero";
+    const getGenre = (id: number): string => {
+        const genre = genresData.genres.find((genre) => genre.id === id);
+        return genre?.name || 'Uknown';
     }
 
     return (
         <div className="show-box">
-            <div>
-                <img src={poster} alt="poster"></img>
-            </div>
-            <div>
-                <div>
-                    <Pill title={getGenre(genreId)} color="red"></Pill>
-                </div>
-                <div>
-                    <p>{title}</p>
-                    <p>* {voteAverage} / 10</p>
+            <div className=" min-w-[100%] overflow-hidden ml-0 bg-[#2f3238] float-none">
+                <img className="h-[370px] relative max-w-none min-w-[100%] overflow-hidden" src={poster} alt="poster"></img>
+                <div className="info">
+                    <div className="text-white pt-[15px] pr-[10px] pb-[15px] pl-[10px] w-[100%] align-middle">
+                        <div>
+                            <Pill title={getGenre(genreId)} color="red"></Pill>
+                        </div>
+                        <p className="block text-[19px] font-bold leading-[27px] mt-[10px] w-[100%]">{title}</p>
+                        <p className="mr-[10px] text-[13px] font-medium table uppercase leading-[20px] w-[100%]">* {voteAverage} / 10</p>
+                    </div>
                 </div>
             </div>
         </div>
