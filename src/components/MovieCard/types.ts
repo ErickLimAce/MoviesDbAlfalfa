cee9ccd94fdf00b938f530cedac5a4e09dd3a8f1
ArturoCarballo <a01662245@tec.mx>
2024-04-15 20:19:41 -0600

    Storybook

--- src/components/MovieCard/types.ts
+++ src/components/MovieCard/types.ts
@@ -1,5 +1,11 @@
 export interface IMovieCard {
+    /**
+     * The title of the movie
+     */
     title: string;
+    /**
+     * The id of the movie genre to get genre
+     */
     genreId: number;
     movieId: number;
     voteAverage: number;
