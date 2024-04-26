837f2a8f613eb658d170c0e4cbf83f7b4d3a82bc
ArturoCarballo <a01662245@tec.mx>
2024-04-25 22:10:56 -0600

    Details

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,22 +5,24 @@ import { ROUTES } from "../../routes/constants";
 
 const Header: React.FC = () => {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to={ROUTES.HOME}>Home</Link>
-                </li>
-                <li>
-                    <Link to={ROUTES.POPULAR}>Popular</Link>
-                </li>
-                <li>
-                    <Link to={ROUTES.TOPRATED}>Top Rated</Link>
-                </li>
-                <li>
-                    <Link to={ROUTES.UPCOMING}>Upcoming</Link>
-                </li>
-            </ul>
-        </nav>
+        <div>
+            <nav>
+                <ul className="flex gap-4 pb-4 justify-center">
+                    <li>
+                        <Link to={ROUTES.HOME}>Home</Link>
+                    </li>
+                    <li>
+                        <Link to={ROUTES.POPULAR}>Popular</Link>
+                    </li>
+                    <li>
+                        <Link to={ROUTES.TOPRATED}>Top Rated</Link>
+                    </li>
+                    <li>
+                        <Link to={ROUTES.UPCOMING}>Upcoming</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     )
 };
 
--- src/pages/Show/Show.tsx
+++ src/pages/Show/Show.tsx
@@ -1,24 +1,59 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useParams, useNavigate } from "react-router-dom";
+import { IDetailsResponse, getDetailsMovies } from "../../services";
+import { IMAGE_SOURCE } from "../../constants/moviesMock";
 
 const Show = () => {
     const { id } = useParams();
     const location = useLocation();
     const navigate = useNavigate();
 
+    const [movie, setMovie] = useState<IDetailsResponse>();
+    const [loading, setLoading] = useState<boolean>(false);
+
     const goBack = () => {
         navigate(-1);
     }
 
+    const getDetails = async () => {
+        const movieId = id ? parseInt(id) : undefined;
+        if (movieId) {
+            await getDetailsMovies(movieId)
+                .then((res) => {
+                    if (res && res.data) {
+                        console.log(res.data, "res");
+                        setMovie(res.data);
+                    }
+                })
+                .catch((err) => {
+                    console.log(err, "err")
+                });
+            setLoading(false);
+        }
+        
+    }
+
     useEffect(() => {
-        //LLamar endpoint de detalles
+        setLoading(true);
+        getDetails();
     }, [])
 
     return (
-        <div>
-            <div>Show id: { id }</div>
-            <div> Titulo desde el state: { location.state.name } </div>
-            <button onClick={goBack}> Ir atras </button>
+        <div className=" mx-44 shadow-2xl rounded-3xl bg-blue-200 ">
+            <div className="p-8">
+                <div>Show id: { id }</div>
+                <div className="flex">
+                    <div className=" min-w-[20%] min-h-[20%]">
+                        <img className="rounded-3xl" src={IMAGE_SOURCE + movie?.poster_path} alt="poster"></img>
+                    </div>
+                    <div className="flex-col px-10">
+                        <p className="font-bold text-3xl pb-4">{movie?.title}</p>
+                        <p>{movie?.overview}</p>
+                        <p>{movie?.release_date}</p>
+                    </div>
+                </div>
+                <button onClick={goBack}> Ir atras </button>
+            </div>
         </div>
     );
 };
